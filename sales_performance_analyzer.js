5148534a5e5e8e0ae8c26e4761393a3047fdd850
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-08 22:09:33 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -3,6 +3,14 @@ function calculateAverageSales(sales)
 { let total = sales.reduce((sum , sale) => sum + sale, 0); // This totals the array buy adding them
     let average = total / sales.length; // This is dividing the total by the amount of entries in the array
     return average }
-    sales = [100, 200, 300, 400]
+
+// Task 2 Create determinePerformanceRating function
+function determinePerformanceRating(averagesales) 
+{if (averagesales > 10000)  {return("Excellent")} 
+else if (averagesales >= 7000)  {return("Good")}
+else if (averagesales >= 4000)  {return("Satisfactory")}
+else if (averagesales >= 0)  {return("Needs Improvement")}
+else {return("Insert sales average.")}}
+    
 
     
